bb069443d72fbcc57157b2114ef650c51401ae7e
Kelsey Aiello <kelseymaiello@gmail.com>
2017-03-06 16:20:33 -0800

    added more js functionality, tweaked input groups

---
+++ app.js
@@ -0,0 +1,15 @@
+strict;
+
+function getUserInput() {
+    var sector = document.getElementById("sector").value;
+    var fin_indep = document.getElementById("fin_indep").value;
+    var pre_degree = document.getElementById("pre_degree").value;
+    var firstgen_pct = document.getElementById("firstgen_pct").value;
+    var fam_income = document.getElementById("fam_income").value;
+
+    alert(sector);
+    alert(fin_indep);
+    alert(pre_degree);
+    alert(firstgen_pct);
+    alert(fam_income);
+}
