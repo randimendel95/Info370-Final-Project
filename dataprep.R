738ea4a11ccdc693ccb5bae4aa2f1803d36ac032
Xiaowen Feng <xfeng1@uw.edu>
2017-03-06 21:18:57 -0800

    csv with control col

--- dataprep.R
+++ dataprep.R
@@ -1,6 +1,6 @@
 library(dplyr)
 #set your working directory
-#setwd('/Users/xiaowenfeng/Desktop/Class/INFO370/Info370-Final-Project')
+setwd('/Users/xiaowenfeng/Desktop/Class/INFO370/Info370-Final-Project')
 
 #read in all csv files
 on.campus.data <- data.frame(read.csv('./data/oncampus_crime.csv', header = TRUE))
@@ -26,8 +26,7 @@ total <- on.campus.col + off.campus.col + public.col
 
 #get all not crime-related columns
 crime.data <- on.campus.data %>%
-  select(UNITID_P,INSTNM,
-         Sector_desc,men_total,
+  select(UNITID_P,INSTNM,men_total,
          women_total,Total)
 #create final data frame
 crime.data <- data.frame(c(crime.data,total))
@@ -39,22 +38,21 @@ crime.data$UNITID <- round(crime.data$UNITID/1000, digits=0)
 
 # aggregate each crime by school
 crime.data <- crime.data %>% group_by(UNITID) %>% 
-  mutate_each(funs(sum), -UNITID, -INSTNM, -Sector_desc, -men_total, -women_total, -Total)
+  mutate_each(funs(sum), -UNITID, -INSTNM, -men_total, -women_total, -Total)
 
 # keep only one row for each unique school id
 crime.data <- subset(crime.data, !duplicated(UNITID))
 View(crime.data)
 #calculate total crime number
-total_crime <- rowSums(crime.data[,7:17])
+total_crime <- rowSums(crime.data[,6:16])
 #create crime rate per 1000 population variable
 crime.data$crime_rate <- total_crime / crime.data$Total * 1000
 View(crime.data)
 crime.data <- crime.data %>%
-  select(UNITID,INSTNM,
-         Sector_desc,crime_rate,Total)
+  select(UNITID,INSTNM,crime_rate,Total)
 
 college.data<- college.data %>%
-  select(UNITID,DEP_STAT_PCT_IND,LOCALE,PREDDEG, PAR_ED_PCT_1STGEN, MD_FAMINC)
+  select(UNITID,CONTROL,DEP_STAT_PCT_IND,PREDDEG, PAR_ED_PCT_1STGEN, MD_FAMINC)
 
 # join college data and crime data 
 final.data <- inner_join(crime.data,college.data, by='UNITID')
@@ -69,19 +67,18 @@ is.na(final.data) <- final.data =="PrivacySuppressed"
 final.data <- na.omit(final.data)
 
 # change names
-colnames(final.data)[3:10] <- c("sector", "crime_rate", "student_pop"
-                                ,"fin_indep", "locale", "pre_degree", "firstgen_pct", "fam_income")
+colnames(final.data)[4:9] <- c( "student_pop","control", 
+                                "fin_indep", "pre_degree", "firstgen_pct", "fam_income")
 # correct data type format
 sapply(final.data,class)
-#remove rows with outlier data
-final.data <- final.data %>% filter(locale != -3)
-final.data <- final.data %>% filter(crime_rate < 1000 & fam_income > 0)
+
 # correct data type format
-final.data$locale <- as.factor(final.data$locale)
 final.data$fin_indep <- as.numeric(as.character(final.data$fin_indep))
 final.data$pre_degree <- as.factor(final.data$pre_degree)
 final.data$firstgen_pct <- as.numeric(as.character(final.data$firstgen_pct))
 final.data$fam_income<- as.numeric(as.character(final.data$fam_income))
+#remove rows with outlier data
+final.data <- final.data %>% filter(crime_rate < 1000 & fam_income > 0)
 View(final.data)
 write.csv(final.data, file = "cleaned.csv")
 
